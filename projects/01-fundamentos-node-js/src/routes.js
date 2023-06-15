85dd1d09a45555caf2882e38d470f2f05abae2d0
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2023-06-14 22:37:08 -0300

    captura dos route parameters usando regex / rota de remoção de um registro do banco

--- projects/01-fundamentos-node-js/src/routes.js
+++ projects/01-fundamentos-node-js/src/routes.js
@@ -1,12 +1,13 @@
 import { randomUUID } from 'node:crypto'
 import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
 
 const database = new Database
 
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (request, response) => {
       const users = database.select('users')
 
@@ -15,7 +16,7 @@ export const routes = [
   },
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (request, response) => {
       const { name, email } = request.body
 
@@ -30,14 +31,15 @@ export const routes = [
       return response.writeHead(201).end()
     }
   },
-
   {
-    method: 'GET',
-    path: '/users',
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
     handler: (request, response) => {
-      const users = database.select('users')
+      const { id } = request.params
 
-      return response.end(JSON.stringify(users))
+      database.delete('users', id)
+
+      return response.writeHead(204).end()
     }
   }
 ]
---
+++ projects/01-fundamentos-node-js/src/utils/build-route-path.js
@@ -0,0 +1,11 @@
+// /users/:id
+export function buildRoutePath(path) {
+
+  //regex para os nomes dos parâmetros
+  const routerParametersRegex = /:([a-zA-z]+)/g
+
+  //regex para os valores válidos dos parâmetros no path
+  const pathWithParams = path.replaceAll(routerParametersRegex, '(?<$1>[a-z0-9\-_]+)')
+
+  return new RegExp(`^${pathWithParams}`)
+}
