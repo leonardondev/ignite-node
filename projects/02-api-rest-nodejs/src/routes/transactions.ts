76ce1f005e7deddc88354439db7ee08fbd7f9a8c
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2023-06-18 18:36:40 -0300

    dev: tipagem do knex e rotas com parâmetros no fastify

--- projects/02-api-rest-nodejs/src/routes/transactions.ts
+++ projects/02-api-rest-nodejs/src/routes/transactions.ts
@@ -5,9 +5,29 @@ import { randomUUID } from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
   app.get('/', async () => {
-    const transaction = await knex('transactions')
+    const transactions = await knex('transactions').select()
 
-    return transaction
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return { transaction }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return { summary }
   })
 
   app.post('/', async (request, reply) => {
