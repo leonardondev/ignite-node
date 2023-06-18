d5fc9189387e6fc5131d00e0a071a600b55c7c82
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2023-06-18 17:08:00 -0300

    dev: rota de cadastro de uma transação

--- projects/02-api-rest-nodejs/src/server.ts
+++ projects/02-api-rest-nodejs/src/server.ts
@@ -1,19 +1,12 @@
 import fastify from 'fastify'
-import { randomUUID } from 'node:crypto'
-import { knex } from './database'
+
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const server = fastify()
 
-server.get('/teste', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: randomUUID(),
-      title: 'Transação de teste',
-      amount: 1000.0,
-    })
-    .returning('*')
-  return transaction
+server.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 server
