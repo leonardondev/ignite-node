e4c2569f777b42547155f0d87b059041e42792e3
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2023-06-21 23:21:48 -0300

    test: testes de integração

---
+++ projects/02-api-rest-nodejs/test/transactions.spec.ts
@@ -0,0 +1,146 @@
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
+import { app } from '../src/app'
+import request from 'supertest'
+import { execSync } from 'node:child_process'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  beforeEach(async () => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
+  it('should be able to create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookie = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookie)
+      .expect(200)
+
+    expect(listTransactionResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ])
+  })
+
+  it('should be able to get a specific transaction', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookie = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookie)
+      .expect(200)
+
+    const transactionId = listTransactionResponse.body.transactions[0].id
+
+    const getTransactionResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookie)
+      .expect(200)
+
+    expect(getTransactionResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    )
+  })
+
+  it('should be able to get the summary', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Credit transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookie = createTransactionResponse.get('Set-Cookie')
+
+    await request(app.server).post('/transactions').set('Cookie', cookie).send({
+      title: 'Debit transaction',
+      amount: 2000,
+      type: 'debit',
+    })
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookie)
+      .expect(200)
+
+    expect(summaryResponse.body.summary).toEqual({
+      amount: 3000,
+    })
+  })
+
+  it('should not be able to list transactions without session id', async () => {
+    const response = await request(app.server).get('/transactions').expect(401)
+
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        error: 'Unauthorized.',
+      }),
+    )
+  })
+
+  it('should not be able to get the summary without session id', async () => {
+    await request(app.server).post('/transactions').send({
+      title: 'Credit transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+
+    await request(app.server).post('/transactions').send({
+      title: 'Debit transaction',
+      amount: 2000,
+      type: 'debit',
+    })
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .expect(401)
+
+    expect(summaryResponse.body).toEqual(
+      expect.objectContaining({
+        error: 'Unauthorized.',
+      }),
+    )
+  })
+})
