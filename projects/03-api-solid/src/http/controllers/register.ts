008ffbe7e68eca6dcd88aa21f8b44e726fdfa2fe
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2023-09-07 15:06:06 -0300

    dev: Manipulação de erros no SOLID

--- projects/03-api-solid/src/http/controllers/register.ts
+++ projects/03-api-solid/src/http/controllers/register.ts
@@ -3,6 +3,7 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { RegisterUseCase } from '@/use-cases/register'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -17,9 +18,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     /* executando use-case */
     const registerUseCase = new RegisterUseCase(new PrismaUsersRepository())
     await registerUseCase.execute({ name, email, password })
-
-    return reply.status(201).send()
   } catch (error) {
-    return reply.status(409).send()
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
+    }
+    throw error
   }
+  return reply.status(201).send()
 }
