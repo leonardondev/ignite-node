8844a8e5321bde9ddc0d7076e37366a88dee7f98
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2023-11-23 23:34:11 -0300

    test: testes e2e

--- projects/03-api-solid/src/http/controllers/authenticate.spec.ts
+++ projects/03-api-solid/src/http/controllers/authenticate.spec.ts
@@ -1,7 +1,31 @@
-import { describe, expect, it } from 'vitest'
+import { app } from '@/app'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
 describe('Authenticate Controller', () => {
-  it('should be able to log', async () => {
-    expect(2 + 2).toEqual(4)
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
   })
 })
