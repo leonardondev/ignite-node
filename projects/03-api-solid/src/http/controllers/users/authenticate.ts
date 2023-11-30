8ab56676f6b37dcaafcbd9637306b5f4a1fbbe46
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2023-11-29 22:57:06 -0300

    feat: refresh token

--- projects/03-api-solid/src/http/controllers/users/authenticate.ts
+++ projects/03-api-solid/src/http/controllers/users/authenticate.ts
@@ -30,7 +30,25 @@ export async function authenticate(
       },
     )
 
-    return reply.status(200).send({ token })
+    const refreshToken = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: '7d',
+        },
+      },
+    )
+
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        httpOnly: true,
+        secure: true, // HTTPS
+        sameSite: true,
+      })
+      .status(200)
+      .send({ token })
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: error.message })
