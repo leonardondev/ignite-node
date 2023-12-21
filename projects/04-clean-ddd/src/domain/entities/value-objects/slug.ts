2e6dc74a016069e6cb628b54f058faae0672239f
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2023-12-20 22:43:53 -0300

    dev: slug

---
+++ projects/04-clean-ddd/src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,28 @@
+export class Slug {
+  public value: string
+
+  constructor(value: string) {
+    this.value = value
+  }
+
+  /**
+   * Receives a string and normalize it as a slug.
+   *
+   * Exemple: "An example title" = "an-exemple-title"
+   *
+   * @param text {string}
+   */
+  static createFromText(text: string) {
+    const slugText = text
+      .normalize('NFKD')
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, '-')
+      .replace(/[^\w-]+/g, '')
+      .replace(/_/g, '-')
+      .replace(/--+/g, '-')
+      .replace(/-$/g, '')
+
+    return new Slug(slugText)
+  }
+}
