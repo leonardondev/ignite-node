1f2e958236c7320827c5de8d4ae542aaab3a41f4
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2024-01-03 23:58:06 -0300

    dev: get question by slug use case

---
+++ projects/04-clean-ddd/src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -0,0 +1,26 @@
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+
+interface GetQuestionBySlugUseCaseRequest {
+  slug: string
+}
+
+interface GetQuestionBySlugUseCaseResponse {
+  question: Question
+}
+
+export class GetQuestionBySlugUseCase {
+  constructor(private questionRepository: QuestionsRepository) {}
+
+  async execute({
+    slug,
+  }: GetQuestionBySlugUseCaseRequest): Promise<GetQuestionBySlugUseCaseResponse> {
+    const question = await this.questionRepository.findBySlug(slug)
+
+    if (!question) {
+      throw new Error('Question not found.')
+    }
+
+    return { question }
+  }
+}
--- projects/04-clean-ddd/test/repositories/in-memory-question-repository.ts
+++ projects/04-clean-ddd/test/repositories/in-memory-question-repository.ts
@@ -4,6 +4,14 @@ import { Question } from '@/domain/forum/enterprise/entities/question'
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
 
+  findBySlug(slug: string) {
+    return new Promise<Question | null>((resolve) => {
+      resolve(
+        this.items.find((question) => question.slug.value === slug) ?? null,
+      )
+    })
+  }
+
   create(question: Question): Promise<Question> {
     return new Promise<Question>((resolve) => {
       this.items.push(question)
