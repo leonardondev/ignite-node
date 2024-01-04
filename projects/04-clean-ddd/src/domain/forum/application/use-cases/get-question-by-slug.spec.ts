9629fb0ae6176666cf1ddfc22e2bb45461368451
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2024-01-04 20:24:03 -0300

    test: factories

--- projects/04-clean-ddd/src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ projects/04-clean-ddd/src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -1,28 +1,20 @@
+import { makeQuestion } from 'test/factories/make-question'
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-question-repository'
 import { GetQuestionBySlugUseCase } from './get-question-by-slug'
-import { DayJsDateService } from 'test/services/dayjs-date-service'
-import { Question } from '../../enterprise/entities/question'
-import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Slug } from '../../enterprise/entities/value-objects/slug'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
-let dateService: DayJsDateService
 let sut: GetQuestionBySlugUseCase
 
 describe('Get Question By Slug', () => {
   beforeEach(() => {
     inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
-    dateService = new DayJsDateService()
     sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
   })
 
   it('should be able to get a question by slug', async () => {
-    const newQuestion = Question.create({
-      authorId: new UniqueEntityID(),
-      title: 'question title 1',
+    const newQuestion = makeQuestion({
       slug: Slug.create('example-question'),
-      content: 'New question content',
-      dateService,
     })
 
     await inMemoryQuestionsRepository.create(newQuestion)
---
+++ projects/04-clean-ddd/test/factories/make-question.ts
@@ -0,0 +1,27 @@
+import { faker } from '@faker-js/faker'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import {
+  Question,
+  QuestionProps,
+} from '@/domain/forum/enterprise/entities/question'
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+import { DayJsDateService } from '../services/dayjs-date-service'
+
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      slug: Slug.create('example-question'),
+      content: faker.lorem.text(),
+      dateService: new DayJsDateService(),
+      ...override,
+    },
+    id,
+  )
+
+  return question
+}
