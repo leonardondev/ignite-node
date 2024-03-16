828df0aa7875f1dbe8691e450fab2258e446caf8
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2024-03-15 22:53:14 -0300

    dev: delete use-cases

--- projects/04-clean-ddd/test/repositories/in-memory-answer-comments-repository.ts
+++ projects/04-clean-ddd/test/repositories/in-memory-answer-comments-repository.ts
@@ -6,10 +6,31 @@ export class InMemoryAnswerCommentsRepository
 {
   public items: AnswerComment[] = []
 
+  findById(id: string): Promise<AnswerComment | null> {
+    return new Promise<AnswerComment | null>((resolve) => {
+      resolve(
+        this.items.find(
+          (answerComment) => answerComment.id.toString() === id,
+        ) ?? null,
+      )
+    })
+  }
+
   create(answerComment: AnswerComment): Promise<AnswerComment> {
     return new Promise<AnswerComment>((resolve) => {
       this.items.push(answerComment)
       resolve(answerComment)
+    })
+  }
+
+  delete(answerComment: AnswerComment): Promise<void> {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === answerComment.id,
+    )
+
+    return new Promise<void>((resolve) => {
+      this.items.splice(itemIndex, 1)
+      resolve()
     })
   }
 }
--- projects/04-clean-ddd/test/repositories/in-memory-question-comments-repository.ts
+++ projects/04-clean-ddd/test/repositories/in-memory-question-comments-repository.ts
@@ -6,10 +6,31 @@ export class InMemoryQuestionCommentsRepository
 {
   public items: QuestionComment[] = []
 
+  findById(id: string): Promise<QuestionComment | null> {
+    return new Promise<QuestionComment | null>((resolve) => {
+      resolve(
+        this.items.find(
+          (questionComment) => questionComment.id.toString() === id,
+        ) ?? null,
+      )
+    })
+  }
+
   create(questionComment: QuestionComment): Promise<QuestionComment> {
     return new Promise<QuestionComment>((resolve) => {
       this.items.push(questionComment)
       resolve(questionComment)
+    })
+  }
+
+  delete(questionComment: QuestionComment): Promise<void> {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === questionComment.id,
+    )
+
+    return new Promise<void>((resolve) => {
+      this.items.splice(itemIndex, 1)
+      resolve()
     })
   }
 }
