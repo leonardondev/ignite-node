805bae0b2e505da4e649ce096b90eedd4c84731c
Leonardo <leonardo.nascimento@sansix.com.br>
2024-04-17 10:26:16 -0300

    chore: setup vitest testing runner with swc compiler

---
+++ projects/05-nest-clean/vitest.config.e2e.ts
@@ -0,0 +1,19 @@
+import swc from 'unplugin-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    include: ['**/*.e2e-spec.ts'],
+    globals: true,
+    root: './src',
+  },
+  plugins: [
+    // This is required to build the test files with SWC
+    swc.vite({
+      // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
+      module: { type: 'es6' },
+    }),
+    tsconfigPaths(),
+  ],
+})
