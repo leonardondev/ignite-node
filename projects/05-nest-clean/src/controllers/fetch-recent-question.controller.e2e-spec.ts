565d8f5fcaa6cbece567a36d547eeda98ccc7552
Leonardo <leonardo.nascimento@sansix.com.br>
2024-04-24 08:25:59 -0300

    test: tests 2e2 no nest com vitest

---
+++ projects/05-nest-clean/src/controllers/fetch-recent-question.controller.e2e-spec.ts
@@ -0,0 +1,70 @@
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import { hash } from 'bcryptjs'
+import request from 'supertest'
+
+describe('Fetch recent questions (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    prisma = moduleRef.get(PrismaService)
+    jwt = moduleRef.get(JwtService)
+
+    app = moduleRef.createNestApplication()
+    await app.init()
+  })
+
+  test('[GET] /questions', async () => {
+    const user = await prisma.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: await hash('123456', 8),
+      },
+    })
+    const accessToken = jwt.sign({ sub: user.id })
+
+    await prisma.question.create({
+      data: {
+        title: 'New question',
+        content: 'Question content',
+        slug: 'new-question',
+        authorId: user.id,
+      },
+    })
+
+    await prisma.question.create({
+      data: {
+        title: 'Another question',
+        content: 'Another question content',
+        slug: 'another-question',
+        authorId: user.id,
+      },
+    })
+
+    const response = await request(app.getHttpServer())
+      .get('/questions')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        title: 'New question',
+        content: 'Question content',
+      })
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toEqual({
+      questions: [
+        expect.objectContaining({ title: 'Another question' }),
+        expect.objectContaining({ title: 'New question' }),
+      ],
+    })
+  })
+})
